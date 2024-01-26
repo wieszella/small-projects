80f87505e19c7b644cc21d34bbc757e0da5024ad
wieszella <99801723+wieszella@users.noreply.github.com>
2024-01-26 17:17:31 +0200

    Add files via upload

--- damka.cpp
+++ damka.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cctype>
 
 const int BOARD_SIZE = 8;
 
@@ -29,7 +30,8 @@ private:
 public:
     CheckersGame() : board(BOARD_SIZE, std::vector<Piece>(BOARD_SIZE, EMPTY)), currentPlayer(PLAYER_1) 
     {
-        initializeBoard();
+        //initializeBoard();
+        initializeBoardDebug();
     }
 
     void play() 
@@ -72,6 +74,20 @@ private:
                 }
             }
         }
+    }
+
+    void initializeBoardDebug()
+    {
+        for (int row = 0; row < BOARD_SIZE; ++row)
+        {
+            for (int col = 0; col < BOARD_SIZE; ++col)
+            {
+                board[row][col] = EMPTY;
+            }
+        }
+        board[0][7] = PLAYER_1_KING;
+        board[4][3] = PLAYER_2_KING;
+        board[5][2] = PLAYER_2_KING;
     }
 
     void printBoard() 
@@ -131,8 +147,8 @@ private:
         std::cout << "Enter move (fromRow fromCol toRow toCol): " << std::endl;
         std::string fromRow = "", fromCol = "", toRow = "", toCol = "";
         //check if input is a single digit
-        while (! (isdigit(fromRow[0]) && isdigit(fromCol[0]) && isdigit(toRow[0]) && isdigit(toCol[0])) && 
-                fromRow.length() == 1 && toRow.length() == 1 && fromCol.length() == 1 && toCol.length() == 1)
+        while (!(isdigit(fromRow[0]) && isdigit(fromCol[0]) && isdigit(toRow[0]) && isdigit(toCol[0])) || 
+                (fromRow.length() != 1 && toRow.length() != 1 && fromCol.length() != 1 && toCol.length() != 1))
         {
             std::cout << "(please enter only digits)" << std::endl;
             std::cin >> fromRow >> fromCol >> toRow >> toCol;
@@ -144,42 +160,31 @@ private:
 
     bool isValidMove(const Move& move) 
     {
-        //check if its a number and in range
+        //check if number in range
         if( move.fromCol<=7 && move.fromCol >=0 &&
             move.fromRow <= 7 && move.fromRow >= 0 &&
             move.toCol <= 7 && move.toCol >= 0 &&
             move.toRow <= 7 && move.toRow >= 0)
             {
+                int playerDirection = currentPlayer == PLAYER_1 ? -1 : 1;
+                Piece opponentKing = currentPlayer == PLAYER_1 ? PLAYER_2_KING : PLAYER_1_KING;
+                Piece opponentPiece = currentPlayer == PLAYER_1 ? PLAYER_2_PIECE : PLAYER_1_PIECE;
                 switch (board[move.fromRow][move.fromCol]) 
                 {
                     case PLAYER_1_PIECE: 
-                        return 
-                            //case not eating move - check that it moves 1 hop in the right direction and that it moves to an empty spot
-                            (move.fromRow == move.toRow - 1 && abs(move.fromCol - move.toCol) == 1 && board[move.toRow][move.toCol] == EMPTY) || 
-                            //case eating move - check that it moves 2 hop in the right direction and that it moves to an empty spot
-                            //AND check that it hops over an opponent's piece
-                            (move.fromRow == move.toRow - 2 && abs(move.fromCol - move.toCol) == 2 && board[move.toRow][move.toCol] == EMPTY) &&
-                            ((board[(move.fromRow + move.toRow) / 2][(move.fromCol + move.toCol)/2] == PLAYER_2_PIECE) ||  
-                            (board[(move.fromRow + move.toRow) / 2][(move.fromCol + move.toCol) / 2] == PLAYER_2_KING))
-                            ? true : false;
-                    
-                    case PLAYER_1_KING:
-                        //check that the king moves in the right diection to an empty spot AND that the king doesnt hop over its own pieces
-                        return (abs(move.fromRow - move.toRow) == abs(move.fromCol - move.toCol) && board[move.toRow][move.toCol] == EMPTY) &&
-                                validKingEatMove(move)
-                                ? true : false;
-                    
                     case PLAYER_2_PIECE:
                         return 
-                            //case not eating
-                            (move.fromRow == move.toRow + 1 && abs(move.fromCol - move.toCol) == 1 && board[move.toRow][move.toCol] == EMPTY) || 
-                            //case eating
-                            (move.fromRow == move.toRow + 2 && abs(move.fromCol - move.toCol) == 2 && board[move.toRow][move.toCol] == EMPTY) &&
-                            ((board[(move.fromRow + move.toRow) / 2][(move.fromCol + move.toCol) / 2] == PLAYER_1_PIECE) ||
-                            (board[(move.fromRow + move.toRow) / 2][(move.fromCol + move.toCol) / 2] == PLAYER_1_KING))
+                            //case not eating move - check that it moves 1 hop in the right direction and that it moves to an empty spot
+                            (move.fromRow == move.toRow + playerDirection && abs(move.fromCol - move.toCol) == 1 && board[move.toRow][move.toCol] == EMPTY) ||
+                            //case eating move - check that it moves 2 hop in the right direction and that it moves to an empty spot
+                            //AND check that it hops over an opponent's piece
+                            (move.fromRow == move.toRow + 2 * playerDirection && abs(move.fromCol - move.toCol) == 2 && board[move.toRow][move.toCol] == EMPTY) &&
+                            ((board[(move.fromRow + move.toRow) / 2][(move.fromCol + move.toCol)/2] == opponentPiece) ||  
+                            (board[(move.fromRow + move.toRow) / 2][(move.fromCol + move.toCol) / 2] == opponentKing))
                             ? true : false;
                    
                     case PLAYER_2_KING:
+                    case PLAYER_1_KING:
                         //check that the king moves in the right diection to an empty spot AND that the king doesnt hop over its own pieces
                         return (abs(move.fromRow - move.toRow) == abs(move.fromCol - move.toCol) && board[move.toRow][move.toCol] == EMPTY) &&
                                 validKingEatMove(move)
@@ -193,19 +198,19 @@ private:
 
     void executeMove(const Move& move) 
     {
-        //this is valid since you wont be able to return backwards if youre not a king so if the 'to' is at the edge it means its a king
+        //check who is making the move
         if (board[move.fromRow][move.fromCol] == PLAYER_1_PIECE || board[move.fromRow][move.fromCol] == PLAYER_1_KING) 
         {
-            //if you moveto an edge row OR you were already a king
-            if (move.toRow == 7 || move.toRow == 0 || board[move.fromRow][move.fromCol] == PLAYER_1_KING)
+            //if you move to an edge row OR you were already a king
+            if (move.toRow == 7 || board[move.fromRow][move.fromCol] == PLAYER_1_KING)
             {
                 board[move.toRow][move.toCol] = PLAYER_1_KING;
             }else{
                 board[move.toRow][move.toCol] = PLAYER_1_PIECE;
             }
         }
-        else {
-            if (move.toRow == 7 || move.toRow == 0 || board[move.fromRow][move.fromCol] == PLAYER_2_KING)
+        else { //must be player 2 considering "move" has already been validated so it cant be empty
+            if (move.toRow == 0 || board[move.fromRow][move.fromCol] == PLAYER_2_KING)
             {
                 board[move.toRow][move.toCol] = PLAYER_2_KING;
             }else{
@@ -224,14 +229,14 @@ private:
 
     bool isGameOver() 
     {
-        int p1flag =0, p2flag = 0;
+        int p1PeiceCounter =0, p2PeiceCounter = 0;
         for (const auto& row : board) {
             for (Piece piece : row) {
-                if (piece == PLAYER_1_PIECE || piece == PLAYER_1_KING) p1flag++;
-                else if (piece == PLAYER_2_PIECE || piece == PLAYER_2_KING) p2flag++;
+                if (piece == PLAYER_1_PIECE || piece == PLAYER_1_KING) p1PeiceCounter++;
+                else if (piece == PLAYER_2_PIECE || piece == PLAYER_2_KING) p2PeiceCounter++;
             }
         }
-        return (p1flag && p2flag) ? false : true;
+        return (p1PeiceCounter && p2PeiceCounter) ? false : true;
     }
 
     Player getWinner() 
@@ -251,35 +256,42 @@ private:
     }
 
     bool validKingEatMove(const Move& move) {
-        if(move.fromRow > move.toRow && move.fromCol > move.toCol)
+        int rowDirection = (move.toRow - move.fromRow) / abs(move.toRow - move.fromRow);
+        int colDirection = (move.toCol - move.fromCol) / abs(move.toCol - move.fromCol);
+
+        for (int i = 1; i < abs(move.fromRow - move.toRow); i++)
         {
-            for (int i = 1; i < abs(move.fromRow - move.toRow); i++)
-            {
+            switch (board[move.fromRow + rowDirection * i][move.fromCol + colDirection * i]) {
+            case PLAYER_1_PIECE:
+            case PLAYER_1_KING:
                 //if one of the pieces between the kings hop was your own piece
-                return (board[move.fromRow - i][move.fromCol - i] == board[move.fromRow][move.fromCol]) ? false : true;
+                if (currentPlayer == PLAYER_1) {
+                    return false;
+                }
+                //case player 2
+                else {
+                    //check that there are no adjacent pieces of the opponent
+                    if (board[move.fromRow + rowDirection * (i + 1)][move.fromCol + colDirection * (i + 1)] == EMPTY
+                        || (board[move.fromRow + rowDirection * i][move.fromCol + colDirection * i]) == EMPTY) break;
+                    else return false;
+                }
+            case PLAYER_2_PIECE:
+            case PLAYER_2_KING:
+                if (currentPlayer == PLAYER_2) {
+                    return false;
+                }
+                //case player 1
+                else {
+                    if (board[move.fromRow + rowDirection * (i + 1)][move.fromCol + colDirection * (i + 1)] == EMPTY ||
+                        (board[move.fromRow + rowDirection * i][move.fromCol + colDirection * i]) == EMPTY)  break;
+                    else return false;
+                }
+                //case empty
+            default:
+                break;
             }
         }
-        else if (move.fromRow < move.toRow && move.fromCol > move.toCol) 
-        {
-            for (int i = 1; i < abs(move.fromRow - move.toRow); i++)
-            {
-                return (board[move.fromRow + i][move.fromCol - i] == board[move.fromRow][move.fromCol]) ? false : true;
-            }
-        }
-        else if (move.fromRow > move.toRow && move.fromCol < move.toCol) 
-        {
-            for (int i = 1; i < abs(move.fromRow - move.toRow); i++)
-            {
-                return (board[move.fromRow - i][move.fromCol + i] == board[move.fromRow][move.fromCol]) ? false : true;
-            }
-        }
-        else 
-        {
-            for (int i = 1; i < abs(move.fromRow - move.toRow); i++)
-            {
-                return (board[move.fromRow + i][move.fromCol + i] == board[move.fromRow][move.fromCol]) ? false : true;
-            }
-        }
+        return true;
     }
 
     void eat(const Move& move) 
